9573111f277ad576eb14c3cd01f30672217fcc19
Martin Hoyer <mhoyer@redhat.com>
2023-03-03 15:11:21 +0100

    Initial changes

--- workday.py
+++ workday.py
@@ -2,25 +2,22 @@
 
 import argparse
 from datetime import datetime, timedelta
-from random import randint
 from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver.support import ui
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import ui
 
-DAY_START = datetime.strptime('07:00', '%H:%M')
-DAY_START_VARIATION = 60  # in minutes
+DAY_START = datetime.strptime('09:00', '%H:%M')
 DAYS = ['mon', 'tue', 'wed', 'thu', 'fri']
 
 
 def generate_times():
-    variation = randint(-DAY_START_VARIATION, DAY_START_VARIATION)
-    start = DAY_START + timedelta(minutes=variation)
-    break_start = start + timedelta(hours=4)
+    start = DAY_START
+    break_start = start + timedelta(hours=3)
     break_end = break_start + timedelta(hours=1)
-    end = break_end + timedelta(hours=4)
+    end = break_end + timedelta(hours=5)
     start_str = start.strftime('%H:%M AM')
     end_str = end.strftime('%H:%M')
     break_start_str = break_start.strftime('%H:%M')
@@ -57,18 +54,27 @@ def main():
         help="Comma-separated list of days to skip (Monday==0 or mon)",
     )
 
+    parser.add_argument(
+        "-m"
+        "--month",
+        dest="month",
+        action='store_true',
+    )
+
     args = parser.parse_args()
 
     now = datetime.now()
+    year = now.year
     week = args.week
 
     if not week:
         year, week, _ = now.isocalendar()
         print("WARNING: Week not specified, using the current one")
 
-    first_day_of_week = datetime.strptime(f'{now.year}-{week-1}-1', "%Y-%U-%w").date()
+    first_day_of_week = datetime.fromisocalendar(year, week, 1)
 
-    print(f"Week: {week}, monday: {first_day_of_week}")
+    if not args.month:
+        print(f"Week: {week}, monday: {first_day_of_week}")
 
     if args.skip:
         days_to_skip = [dayint(d.strip()) for d in args.skip.split(",")]
@@ -79,48 +85,53 @@ def main():
 
     br = webdriver.Firefox()
     br.implicitly_wait(2)
-    wait = ui.WebDriverWait(br, 60)
+    wait = ui.WebDriverWait(br, 10)
 
-    br.get('https://rover.redhat.com/apps/')
+    br.get('https://wd5.myworkday.com/redhat/d/home.htmld')
 
-    link_to_wd = br.find_element("xpath", "//a[contains(@title, 'Performance & development, career management, onboarding, compensation, and more.')]")
-    link_to_wd.click()
-
-    sleep(3)
-
-    br.switch_to.window(br.window_handles[-1])
+    sleep(1)
 
     wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@title='Global Navigation']"))).click()
     wait.until(ec.element_to_be_clickable((By.XPATH, "//span[text()='Time']"))).click()
     wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@title='Select Week']"))).click()
-    wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'input')]"))).send_keys(str(first_day_of_week.month).zfill(2) + str(first_day_of_week.day).zfill(2) + str(first_day_of_week.year))
+    day_to_send = str(first_day_of_week.month).zfill(2) + str(first_day_of_week.day).zfill(2) + str(first_day_of_week.year)
+    if args.month:
+        day_to_send = str(now.month).zfill(2) + str(1).zfill(2) + str(year)
+    wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'input')]"))).send_keys(day_to_send)
     wait.until(ec.element_to_be_clickable((By.XPATH, "//span[@title='OK']"))).click()
-    wait.until(ec.element_to_be_clickable((By.XPATH, "//span[@title='Actions']"))).click()
     sleep(1)
-    wait.until(ec.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Quick Add')]"))).click()
-    sleep(2)
-    wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@title='Next']"))).click()
-    wait.until(ec.element_to_be_clickable((By.XPATH, "//span[@title='Add']"))).click()
-    sleep(1)
-    inputs = br.find_elements("xpath", "//input[@type='text']")
+    for i in range(5):
+        print(f"Working on week {i+1}")
+        if i != 0:
+            wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@data-automation-id='nextMonthButton']"))).click()
+            sleep(1)
+        wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@title='Actions']"))).click()
+        wait.until(ec.element_to_be_clickable((By.XPATH, "//div[@data-automation-label='Quick Add']"))).click()
+        sleep(1)
+        wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@title='Next']"))).click()
+        wait.until(ec.element_to_be_clickable((By.XPATH, "//span[@title='Add']"))).click()
+        sleep(1)
+        inputs = br.find_elements("xpath", "//input[@type='text']")
 
-    times = generate_times()
+        times = generate_times()
 
-    for input, time in zip(inputs[:4], times):
-        input.send_keys(time)
+        for input, time in zip(inputs[:4], times):
+            input.send_keys(time)
 
-    br.find_element("xpath", "//div[@data-automation-id='selectShowAll']").click()
+        br.find_element("xpath", "//div[@data-automation-id='selectShowAll']").click()
 
-    br.find_element("xpath", "//div[@title='Break']").click()
+        br.find_element("xpath", "//div[@title='Break']").click()
 
-    days = br.find_elements("xpath", "//div[@data-automation-id='checkboxPanel']")
+        days = br.find_elements("xpath", "//div[@data-automation-id='checkboxPanel']")
 
-    for index, day in enumerate(days[:-2]):
-        if index in days_to_skip:
-            continue
-        day.click()
+        for index, day in enumerate(days[:-2]):
+            if index in days_to_skip:
+                continue
+            day.click()
 
-    br.find_element("xpath", "//button[@title='OK']").click()
+        br.find_element("xpath", "//button[@title='Cancel']").click()
+        if not args.month:
+            break
 
 
 if __name__ == "__main__":
